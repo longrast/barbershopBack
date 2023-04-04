ccb6ae3e3e553ddf502fbc3e400892c540dfa68c
Nikita Kulichev <longrast.2002@gmail.com>
2023-04-05 02:35:53 +0300

    authorization

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import sqlite3
-from flask import *
-#Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import *
-#abort
+from flask import Flask, render_template, request, url_for, flash, redirect, session
+from werkzeug.exceptions import abort
+from datetime import timedelta
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -11,23 +10,82 @@ def get_db_connection():
 
 
 
-from flask import Flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+app.secret_key = 'verystrongsecretkey'
+app.permanent_session_lifetime = timedelta(minutes=1)
 
-#import re
-#from datetime import datetime
-
-#from flask import send_from_directory
-#from flask import Flask, render_template
+#-------------------------------------------------------------------------------------------------------------
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
-@app.route("/authorization")
+#-------------------------------------------------------------------------------------------------------------
+
+@app.route("/authorization", methods=('GET', 'POST'))
 def authorization():
+    if request.method == 'POST':
+        email = request.form['email']
+        pswd = request.form['pswd']
+        if not email or not pswd:
+            flash('Пожалуйста, заполните формы')
+        else:
+            #session["name"] = first_name
+            conn = get_db_connection()
+            table_user_id = conn.execute('SELECT user_id FROM user where email = ?', (email,)).fetchone()['user_id']
+            table_first_name = conn.execute('SELECT first_name FROM user where email = ?', (email,)).fetchone()['first_name']
+            table_second_name = conn.execute('SELECT second_name FROM user where email = ?', (email,)).fetchone()['second_name']
+            table_pswd = conn.execute('SELECT pswd FROM user where email = ?', (email,)).fetchone()['pswd']
+            #check = conn.execute('SELECT * FROM user where email = ?', (email,)).fetchone()['pswd']
+            conn.close()
+            if not table_user_id:
+                flash('Такая почта не найдена!')
+                conn.close()
+                return redirect(url_for('authorization'))
+            else:
+                #print(table_pswd)
+                #print(pswd)
+                #print(check)
+                if table_pswd == pswd:
+                    session["user_id"] = table_user_id
+                    session["first_name"] = table_first_name
+                    session["second_name"] = table_second_name
+                else:
+                    flash('Неверный пароль')
+                return redirect(url_for('authorization'))
     return render_template('authorization.html')
+
+#-------------------------------------------------------------------------------------------------------------
+
+@app.route("/registration", methods=('GET', 'POST'))
+def registration():
+    #flash('Ок')
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        second_name = request.form['second_name']
+        age = request.form['age']
+        email = request.form['email']
+        pswd = request.form['pswd']
+
+        if not email or not pswd:
+            flash('Пожалуйста, заполните формы')
+        else:
+            #session["name"] = first_name
+            conn = get_db_connection()
+            table_user_id = conn.execute('SELECT user_id FROM user where email = ?', (email,)).fetchone()
+            if table_user_id:
+                conn.close()
+                flash('Вы уже зарегистрированы!')
+                return redirect(url_for('authorization')) #добавить всплывающее окно flash, что пользователь уже имеет акк
+            conn.execute('INSERT INTO user (first_name, second_name, age, email, pswd) VALUES (?, ?, ?, ?, ?)',
+                         (first_name, second_name, age, email, pswd))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('registration'))
+    return render_template('registration.html')
+
+#-------------------------------------------------------------------------------------------------------------
+
 
 @app.route("/cart")
 def cart():
@@ -53,26 +111,6 @@ def portfolio_card():
 def portfolio():
     return render_template('portfolio.html')
 
-@app.route("/registration", methods=('GET', 'POST'))
-def registration():
-    flash('Ок')
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        second_name = request.form['second_name']
-        age = request.form['age']
-        email = request.form['email']
-        pswd = request.form['pswd']
-        if not email or not pswd:
-            flash('Пожалуйста, заполните формы')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO user (first_name, second_name, age, email, pswd) VALUES (?, ?, ?, ?, ?)',
-                         (first_name, second_name, age, email, pswd))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('registration'))
-    return render_template('registration.html')
-
 @app.route("/restore_password")
 def restore_password():
     return render_template('restore-password.html')
@@ -85,6 +123,16 @@ def service_and_price():
 @app.route("/service_events")
 def service_events():
     return render_template('service_events.html')
+
+
+@app.route("/hello")
+def hello():
+    if "first_name" in session:
+        temp = session["first_name"]
+        content = "hello, " + temp
+    else:
+        content = "hello, stranger"
+    return content
 
 '''
 @app.route("/hello/<name>")
