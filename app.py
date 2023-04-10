1fe0d46713b678809d6cdbb0395be2bff6268354
Nikita Kulichev <longrast.2002@gmail.com>
2023-04-11 01:05:21 +0300

    com

--- app.py
+++ app.py
@@ -8,17 +8,46 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-
-
 app = Flask(__name__)
 app.secret_key = 'verystrongsecretkey'
-app.permanent_session_lifetime = timedelta(minutes=1)
+app.permanent_session_lifetime = timedelta(minutes=2)
 
 #-------------------------------------------------------------------------------------------------------------
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+#-------------------------------------------------------------------------------------------------------------
+
+@app.route("/registration", methods=('GET', 'POST'))
+def registration():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        second_name = request.form['second_name']
+        age = request.form['age']
+        number = request.form['number']
+        email = request.form['email']
+        pswd = request.form['pswd']
+        if not email or not pswd:
+            flash('Пожалуйста, заполните формы')
+        else:
+            #session["name"] = first_name
+            conn = get_db_connection()
+            check_table_user_id = conn.execute('SELECT user_id FROM user where email = ?', (email,)).fetchone()
+            if check_table_user_id:
+                conn.close()
+                flash('Вы уже зарегистрированы!')
+                return redirect(url_for('authorization'))
+            conn.execute('INSERT INTO user (first_name, second_name, age, number, email, pswd) VALUES (?, ?, ?, ?, ?, ?)',
+                (first_name, second_name, age, number, email, pswd))
+            table_user_id = conn.execute('SELECT user_id FROM user where email = ?', (email,)).fetchone()['user_id']
+            conn.execute("INSERT INTO access (access_id, role) VALUES (?, ?)",
+                (table_user_id, '2'))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('authorization'))
+    return render_template('registration.html')
 
 #-------------------------------------------------------------------------------------------------------------
 
@@ -40,6 +69,9 @@ def authorization():
                 table_user_id = conn.execute('SELECT user_id FROM user where email = ?', (email,)).fetchone()['user_id']
                 table_first_name = conn.execute('SELECT first_name FROM user where email = ?', (email,)).fetchone()['first_name']
                 table_second_name = conn.execute('SELECT second_name FROM user where email = ?', (email,)).fetchone()['second_name']
+                table_age = conn.execute('SELECT age FROM user where email = ?', (email,)).fetchone()['age']
+                table_number = conn.execute('SELECT number FROM user where email = ?', (email,)).fetchone()['number']
+                table_email = conn.execute('SELECT email FROM user where email = ?', (email,)).fetchone()['email']
                 table_pswd = conn.execute('SELECT pswd FROM user where email = ?', (email,)).fetchone()['pswd']
                 #check = conn.execute('SELECT * FROM user where email = ?', (email,)).fetchone()['pswd']
                 conn.close()
@@ -50,46 +82,17 @@ def authorization():
                     session["user_id"] = table_user_id
                     session["first_name"] = table_first_name
                     session["second_name"] = table_second_name
+                    session["age"] = table_age
+                    session["number"] = table_number
+                    session["email"] = table_email
+                    session["pswd"] = table_pswd
+                    session.permanent = True
                 else:
                     flash('Неверный пароль')
-                return redirect(url_for('authorization'))
+                return redirect(url_for('home'))
     return render_template('authorization.html')
 
 #-------------------------------------------------------------------------------------------------------------
-
-@app.route("/registration", methods=('GET', 'POST'))
-def registration():
-    #flash('Ок')
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        second_name = request.form['second_name']
-        age = request.form['age']
-        number = request.form['number']
-        email = request.form['email']
-        pswd = request.form['pswd']
-
-        if not email or not pswd:
-            flash('Пожалуйста, заполните формы')
-        else:
-            #session["name"] = first_name
-            conn = get_db_connection()
-            check_table_user_id = conn.execute('SELECT user_id FROM user where email = ?', (email,)).fetchone()
-            if check_table_user_id:
-                conn.close()
-                flash('Вы уже зарегистрированы!')
-                return redirect(url_for('authorization')) #добавить всплывающее окно flash, что пользователь уже имеет акк
-            conn.execute('INSERT INTO user (first_name, second_name, age, number, email, pswd) VALUES (?, ?, ?, ?, ?, ?)',
-                (first_name, second_name, age, number, email, pswd))
-            table_user_id = conn.execute('SELECT user_id FROM user where email = ?', (email,)).fetchone()['user_id']
-            conn.execute("INSERT INTO access (access_id, role) VALUES (?, ?)",
-                (table_user_id, '2'))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('registration'))
-    return render_template('registration.html')
-
-#-------------------------------------------------------------------------------------------------------------
-
 
 @app.route("/cart")
 def cart():
@@ -130,12 +133,22 @@ def service_events():
 
 @app.route("/profile")
 def profile():
-    return render_template('profile.html')
+    if session:
+        return render_template('profile.html')
+    else:
+        return abort(404)
 
 @app.route("/profile_edit")
 def profile_edit():
-    return render_template('profile_edit.html')
+    if session:
+        return render_template('profile_edit.html')
+    else:
+        return abort(404)
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
 @app.route("/hello")
 def hello():
